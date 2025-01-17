9037477555f31dab0d2e0072052b47d4d2f56d2c
gonjila <lgonjila@gmail.com>
2025-01-18 03:32:36 +0400

    feat: translate cards texts

--- resources/js/Components/CategoryItem.vue
+++ resources/js/Components/CategoryItem.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
     import {Link} from '@inertiajs/vue3'
-    import {ref} from "vue";
+    import {computed, ref} from "vue";
+    import {useI18n} from "vue-i18n";
 
-    export type DataType = {id: string, name: string}
+    export type DataType = {id: string, title: string}
 
-    defineProps<{
+    const {data} = defineProps<{
         data: DataType
     }>()
 
+    const i18n = useI18n()
     const mouseEntered = ref(false)
 
     const mouseOverHandler = (value: boolean) => {
         mouseEntered.value = value
     }
+
+    const translatedName = computed(() => {
+        return data.title[i18n.locale.value]
+    })
 </script>
 
 <template>
@@ -23,12 +29,12 @@
         <Link :href="route('all-products')">
             <img
                 src="https://bonee.blob.core.windows.net/images/1869aa84-ab63-c4c5-ef95-23d7d07e30d8_3.webp"
-                :alt="data.name"
+                :alt="translatedName"
                 :class="`absolute inset-0 transition duration-500 ${mouseEntered && 'scale-110'}`"
             >
 
-            <p class="text-center text-white font-bold uppercase w-full bg-black/50 py-3 absolute left-0 bottom-0 z-10">
-                {{data.name}}
+            <p class="text-center text-white font-bold uppercase w-full bg-black/50 p-3 absolute left-0 bottom-0 z-10">
+                {{translatedName}}
             </p>
         </Link>
     </li>
--- resources/js/Components/ProductItem.vue
+++ resources/js/Components/ProductItem.vue
@@ -1,63 +1,69 @@
 <script setup lang="ts">
-import {Link} from '@inertiajs/vue3';
-import {computed, ref} from "vue";
-import {useI18n} from 'vue-i18n'
+    import {Link} from '@inertiajs/vue3';
+    import {computed, ref} from "vue";
+    import {useI18n} from 'vue-i18n'
 
-export type DataType = {
-    id: string;
-    name: string;
-    image:string;
-    description: string;
-    price: number;
-    discount: number;
-    discounted_price: number;
-}
-
-const {data} = defineProps<{
-    data: DataType
-}>()
-
-const i18n = useI18n()
-const isMouseEntered = ref(false)
-
-const mouseOverHandler = (value: boolean) => {
-    isMouseEntered.value = value
-}
-
-const computedDiscountPrice = computed(()=> {
-    if(!data.discount && !data.discounted_price){
-        return null
-    }else if (data.discount && !data.discounted_price){
-        return data.price * data.discount / 100
+    interface ITranslations {
+        en: string;
+        ka: string;
     }
 
-    return data.discounted_price
-})
+    export type DataType = {
+        id: string;
+        name: ITranslations;
+        image:string;
+        description: ITranslations;
+        price: number;
+        discount: number;
+        discounted_price: number;
+    }
+
+    const {data} = defineProps<{
+        data: DataType
+    }>()
+
+    const i18n = useI18n()
+    const isMouseEntered = ref(false)
+
+    const mouseOverHandler = (value: boolean) => {
+        isMouseEntered.value = value
+    }
+
+    const computedDiscountPrice = computed(()=> {
+        if(!data.discount && !data.discounted_price){
+            return null
+        }else if (data.discount && !data.discounted_price){
+            return data.price * data.discount / 100
+        }
+
+        return data.discounted_price
+    })
 
     const translatedName = computed(() => {
         return data.name[i18n.locale.value]
     })
-
-console.log({image: data.image})
+    const translatedDescription = computed(() => {
+        return data.description[i18n.locale.value]
+    })
 </script>
 
 <template>
     <li class="relative boxBg boxShadow !overflow-visible cursor-pointer">
-        <Link :href="route('product', {productName: translatedText || ''})" class="h-full flex flex-col">
+        <Link :href="route('product', {productName: translatedName || ''})" class="h-full flex flex-col">
             <div
                 @mouseenter="mouseOverHandler(true)" @mouseleave="mouseOverHandler(false)"
                 class="aspect-[4/3] rounded-t-lg overflow-hidden flex items-start justify-center flex-1"
             >
                 <img
                     :src="data.image"
-                    :alt="data.name.en"
+                    :alt="translatedName"
                     :class="` transition duration-500 ${isMouseEntered && 'scale-110'}`"
                 >
             </div>
 
             <div class="pt-4 px-6 pb-8 flex flex-col gap-4 relative">
-                <h2 class="blackText text-center text-lg font-bold uppercase">{{data.name.en}}</h2>
-                <p class="line-clamp-3">{{data.description.en}}</p>
+                <h2 class="blackText text-center text-lg font-bold uppercase">{{translatedName}}</h2>
+                <p class="line-clamp-3">{{translatedDescription}}</p>
 
                 <div class="py-1 lg:py-2 px-5 lg:px-10 bg-gray-300 dark:bg-gray-500 rounded-lg flex items-center gap-3 absolute -bottom-5 left-1/2 -translate-x-1/2">
                     <div class="flex gap-2 items-center">
