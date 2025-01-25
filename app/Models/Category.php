cf1e9afffb0c130d6dc22bf3acb44bd9913f59cd
gonjila <lgonjila@gmail.com>
2025-01-26 02:03:03 +0400

    feat: delete or update category images in storage

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Database\Factories\CategoryFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
@@ -18,5 +19,26 @@ class Category extends Model
 
     function products(): BelongsToMany {
         return $this->belongsToMany(Product::class);
+    }
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::deleting(function ($product) {
+            if ($product->image) {
+                Storage::disk('public')->delete($product->image);
+            }
+        });
+
+        static::updating(function ($product) {
+            // Check if the image is being updated
+            if ($product->isDirty('image')) {
+                $oldImage = $product->getOriginal('image');
+                if ($oldImage) {
+                    Storage::disk('public')->delete($oldImage);
+                }
+            }
+        });
     }
 }
