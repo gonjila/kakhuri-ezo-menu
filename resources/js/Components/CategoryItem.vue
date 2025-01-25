ec03b0b85f71ada137f9a669d6f1db620dc6fac3
gonjila <lgonjila@gmail.com>
2025-01-25 19:22:00 +0400

    feat: route into products page with category_id query

--- resources/js/Components/CategoryItem.vue
+++ resources/js/Components/CategoryItem.vue
@@ -20,7 +20,7 @@
         @mouseenter="mouseOverHandler(true)" @mouseleave="mouseOverHandler(false)"
         class="boxBg boxShadow w-full md:w-[300px] aspect-video md:aspect-square !rounded-[50px] relative overflow-hidden"
     >
-        <Link :href="route('all-products')">
+        <Link :href="route('all-products', {category_id: data.id})">
             <img
                 src="https://bonee.blob.core.windows.net/images/1869aa84-ab63-c4c5-ef95-23d7d07e30d8_3.webp"
                 :alt="data.title"
