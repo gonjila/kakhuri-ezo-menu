9f64c03000512706285c7b35e2f252a2aad7ab34
gonjila <lgonjila@gmail.com>
2025-01-25 19:57:01 +0400

    feat: render categories dynamic images

--- resources/js/Components/CategoryItem.vue
+++ resources/js/Components/CategoryItem.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import {Link} from '@inertiajs/vue3'
-    import {ref} from "vue";
+    import {computed, ref} from "vue";
     import {ICategory} from "@/types";
 
 
@@ -13,18 +13,24 @@
     const mouseOverHandler = (value: boolean) => {
         mouseEntered.value = value
     }
+
+    const imageUrl = computed(() => {
+        if(data.image.startsWith("http")){
+            return data.image;
+        }
+
+        return import.meta.env.BASE_URL + "storage/" + data.image;
+    })
 </script>
+<!--                src="https://bonee.blob.core.windows.net/images/1869aa84-ab63-c4c5-ef95-23d7d07e30d8_3.webp"-->
 
 <template>
-    <li
-        @mouseenter="mouseOverHandler(true)" @mouseleave="mouseOverHandler(false)"
-        class="boxBg boxShadow w-full md:w-[300px] aspect-video md:aspect-square !rounded-[50px] relative overflow-hidden"
-    >
+    <li class="boxBg boxShadow w-full md:w-[300px] aspect-video md:aspect-square !rounded-[50px] relative overflow-hidden">
         <Link :href="route('all-products', {category_id: data.id})">
             <img
-                src="https://bonee.blob.core.windows.net/images/1869aa84-ab63-c4c5-ef95-23d7d07e30d8_3.webp"
+                :src="imageUrl"
                 :alt="data.title"
-                :class="`absolute inset-0 transition duration-500 ${mouseEntered && 'scale-110'}`"
+                class="w-full h-full object-cover transition duration-500 hover:scale-110"
             >
 
             <p class="text-center text-white font-bold uppercase w-full bg-black/50 p-3 absolute left-0 bottom-0 z-10">
