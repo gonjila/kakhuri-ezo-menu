a4f168bce16b7ad11e196f3762716142d351a608
gonjila <lgonjila@gmail.com>
2025-02-08 05:42:13 +0400

    refactor: delete extra auth files

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,3 @@ Route::prefix('products')->controller(ProductController::class)->group(function 
 
     Route::get('/{productId}', 'get')->name('product');
 });
-
-//Route::get('/dashboard', function () {
-//    return Inertia::render('Dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
-
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//});
-
-//require __DIR__.'/auth.php';
